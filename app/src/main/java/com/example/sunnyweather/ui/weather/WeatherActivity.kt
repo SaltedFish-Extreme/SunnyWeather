4f7c2d4c286881abe4d6e41aac752260eee418ac
xianyuzhizun <1738099872@qq.com>
2021-11-15 22:02:14 +0800

    <feat> 天气查询UI层页面布局实现

---
+++ app/src/main/java/com/example/sunnyweather/ui/weather/WeatherActivity.kt
@@ -0,0 +1,12 @@
+package com.example.sunnyweather.ui.weather
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.sunnyweather.R
+
+class WeatherActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_weather)
+    }
+}
